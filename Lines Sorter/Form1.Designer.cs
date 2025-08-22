a3cb5697a2835db6cab1bf988f982931c0ef88ea
Admin <Admin@ADMIN-PC>
2025-08-22 19:41:15 +0700

    Add project files.

---
+++ Lines Sorter/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Lines_Sorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.lblFileCount = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 68);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(287, 264);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(293, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Sorter";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(12, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 91);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F);
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(168, 347);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 91);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(314, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(431, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Text to Sort (seperate words with | to search multiple terms):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(318, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(470, 26);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(318, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(470, 101);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sort";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SaveFileDialog
+            // 
+            this.SaveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // lblFileCount
+            // 
+            this.lblFileCount.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lblFileCount.AutoSize = true;
+            this.lblFileCount.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblFileCount.Location = new System.Drawing.Point(314, 120);
+            this.lblFileCount.Name = "lblFileCount";
+            this.lblFileCount.Size = new System.Drawing.Size(155, 20);
+            this.lblFileCount.TabIndex = 8;
+            this.lblFileCount.Text = "Processing file 0 of 0";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblStatus.Location = new System.Drawing.Point(314, 140);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(189, 20);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "Processing: Nothing... 0%";
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.progressBar1.Location = new System.Drawing.Point(318, 163);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(470, 62);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(318, 338);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(470, 101);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.checkBox1.Location = new System.Drawing.Point(647, 22);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(141, 24);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Case-Sensitive";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblFileCount);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog SaveFileDialog;
+        private System.Windows.Forms.Label lblFileCount;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Lines Sorter/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lines_Sorter
+{
+    public partial class Form1 : Form
+    {
+        private System.Threading.CancellationTokenSource _cancellationTokenSource;
+        public Form1()
+        {
+            InitializeComponent();
+            this.listBox1.AllowDrop = true;
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // If it is, allow the drop by setting the effect to Copy. [14]
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            // Add each file path to the list box. [16]
+            foreach (string file in files)
+            {
+                listBox1.Items.Add(file);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            // Allow the user to select multiple files
+            openFileDialog.Multiselect = true;
+            // Set the filter to show all file types
+            openFileDialog.Filter = "All files (*.*)|*.*";
+
+            // Show the dialog and if the user clicks OK, add the selected file paths to the list box
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Check if any items are selected in the list box.
+            if (listBox1.SelectedItems.Count > 0)
+            {
+                // Create a temporary list to hold the items to be removed to avoid issues with modifying the collection while iterating over it.
+                var selectedItems = new System.Collections.ArrayList(listBox1.SelectedItems);
+                // Remove each selected item from the list box. [9]
+                foreach (var item in selectedItems)
+                {
+                    listBox1.Items.Remove(item);
+                }
+            }
+            else
+            {
+                // If no item is selected, show a message to the user.
+                MessageBox.Show("Please select at least one file to remove.", "No File Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+            // Check if there are any file paths listed in the ListBox.
+private async void button2_Click(object sender, EventArgs e)
+        {
+            // --- (Initial checks and search term preparation are correct and remain the same) ---
+            if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("Please add file paths to the list box.", "No Files Listed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string searchText = textBox1.Text;
+            if (string.IsNullOrWhiteSpace(searchText))
+            {
+                MessageBox.Show("Please enter the text to search for.", "Search Text Missing", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string[] searchTerms = searchText.Split('|')
+                                             .Select(term => term.Trim())
+                                             .Where(term => !string.IsNullOrEmpty(term))
+                                             .ToArray();
+            if (searchTerms.Length == 0)
+            {
+                MessageBox.Show("The search text is invalid.", "Invalid Search Text", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            var comparisonMode = checkBox1.Checked
+                ? StringComparison.Ordinal
+                : StringComparison.OrdinalIgnoreCase;
+
+            // --- (Initialization and UI setup are correct and remain the same) ---
+            _cancellationTokenSource = new System.Threading.CancellationTokenSource();
+            var token = _cancellationTokenSource.Token;
+            int totalFiles = listBox1.Items.Count;
+            int processedFiles = 0;
+            var linesToKeep = new System.Collections.Generic.List<string>();
+            SetUiStateForProcessing(true);
+            progressBar1.Value = 0;
+            lblFileCount.Text = "Starting...";
+            lblStatus.Text = "";
+
+            try
+            {
+                await System.Threading.Tasks.Task.Run(() =>
+                {
+                    foreach (var item in listBox1.Items)
+                    {
+                        if (token.IsCancellationRequested) break;
+
+                        processedFiles++;
+                        string filePath = item.ToString();
+                        string fileName = System.IO.Path.GetFileName(filePath);
+                        string displayName = fileName.Length > 20 ? fileName.Substring(0, 20) + "..." : fileName;
+
+                        this.Invoke((System.Action)delegate {
+                            lblFileCount.Text = $"Processing file {processedFiles} of {totalFiles}";
+                            progressBar1.Value = 0; // Reset for the new file
+                        });
+
+                        try
+                        {
+                            if (!System.IO.File.Exists(filePath))
+                            {
+                                this.Invoke((System.Action)delegate { lblStatus.Text = $"File not found: {displayName}"; });
+                                continue;
+                            }
+
+                            var fileInfo = new System.IO.FileInfo(filePath);
+                            long totalBytes = fileInfo.Length;
+
+                            using (var fs = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                            using (var sr = new System.IO.StreamReader(fs))
+                            {
+                                string line;
+                                // *** MODIFIED: Set up for Balanced update ***
+                                long lineCount = 0;
+                                const int reportInterval = 100000; // Update every 100,000 lines
+
+                                while ((line = sr.ReadLine()) != null)
+                                {
+                                    if (token.IsCancellationRequested) break;
+
+                                    foreach (var term in searchTerms)
+                                    {
+                                        if (line.IndexOf(term, comparisonMode) >= 0)
+                                        {
+                                            linesToKeep.Add(line);
+                                            break;
+                                        }
+                                    }
+
+                                    // *** MODIFIED: The "Balanced" progress update logic ***
+                                    lineCount++;
+                                    if (lineCount % reportInterval == 0)
+                                    {
+                                        // Still use file position for an accurate percentage when we do update
+                                        int percentage = (totalBytes > 0) ? (int)((double)fs.Position * 100 / totalBytes) : 0;
+                                        this.Invoke((System.Action)delegate {
+                                            progressBar1.Value = percentage;
+                                            lblStatus.Text = $"Processing: {displayName}... {percentage}%";
+                                        });
+                                    }
+                                }
+                            }
+                            // Add a final update to show 100% for the completed file
+                            this.Invoke((System.Action)delegate { progressBar1.Value = 100; });
+                        }
+                        catch (System.Exception ex)
+                        {
+                            System.Diagnostics.Debug.WriteLine($"An error occurred processing {filePath}: {ex.Message}");
+                            this.Invoke((System.Action)delegate { lblStatus.Text = $"Error on: {displayName}"; });
+                        }
+                    }
+                }, token);
+            }
+            finally
+            {
+                SetUiStateForProcessing(false);
+                _cancellationTokenSource.Dispose();
+            }
+
+            // --- Post-Processing Logic ---
+            if (token.IsCancellationRequested)
+            {
+                lblStatus.Text = "Operation Cancelled.";
+                if (linesToKeep.Count > 0)
+                {
+                    var result = MessageBox.Show(
+                        "The process was cancelled. Do you want to save the results found so far?",
+                        "Save Partial Results?",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        SaveResults(linesToKeep);
+                    }
+                }
+            }
+            else // Process completed normally
+            {
+                lblFileCount.Text = $"Processed {processedFiles} of {totalFiles} files.";
+                lblStatus.Text = "Processing complete.";
+                progressBar1.Value = 100;
+
+                if (linesToKeep.Count > 0)
+                {
+                    SaveResults(linesToKeep);
+                }
+                else
+                {
+                    MessageBox.Show("No lines containing the specified text were found.", "No Matches Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+        private void SetUiStateForProcessing(bool isProcessing)
+        {
+            button2.Enabled = !isProcessing;
+            button4.Enabled = isProcessing;
+            listBox1.Enabled = !isProcessing;
+            textBox1.Enabled = !isProcessing;
+            checkBox1.Enabled = !isProcessing;
+        }
+
+        // Helper method to handle saving the file, avoiding code duplication.
+        private void SaveResults(System.Collections.Generic.List<string> lines)
+        {
+            using (var saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog.Title = "Save the processed file";
+                saveFileDialog.FileName = "filtered_output.txt";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        System.IO.File.WriteAllLines(saveFileDialog.FileName, lines);
+                        MessageBox.Show("File saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        lblStatus.Text = "File saved successfully.";
+                    }
+                    catch (System.Exception ex)
+                    {
+                        MessageBox.Show($"An error occurred while saving the file:\n\n{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        lblStatus.Text = "Error saving file.";
+                    }
+                }
+                else
+                {
+                    lblStatus.Text = "Save operation cancelled.";
+                }
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (_cancellationTokenSource != null)
+            {
+                _cancellationTokenSource.Cancel();
+                button4.Enabled = false; // Disable the button to prevent multiple clicks.
+            }
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
